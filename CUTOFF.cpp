a666552b5601c61472c2e86c87e026913488a2e1
Khushi Pal <65907675+khushi06-svg@users.noreply.github.com>
2025-02-08 00:46:50 +0530

    Create CUTOFF.cpp
    Solution for the problem CUTOFF

---
+++ CUTOFF.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        sort(a, a + n);
+
+        int passmark = a[n-x]-1;
+        cout << passmark << endl;
+    }
+
+    return 0;
+}
