822270cc27e13d5b825d6fd1570970f5001c94ae
Khushi Pal <65907675+khushi06-svg@users.noreply.github.com>
2025-02-17 00:09:47 +0530

    Create CREDCOINS.cpp
    Solution for the problem CREDCOINS

---
+++ CREDCOINS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; cin>>t;
+	while(t--){
+	    int x,y; cin>>x>>y;
+	    int c = x*y;
+	    cout<<c/100<<endl;
+	}
+	return 0;
+}
