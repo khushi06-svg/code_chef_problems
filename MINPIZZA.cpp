a3a62f4e35e679562bbf2f1b9891b979fa166b9e
Khushi Pal <65907675+khushi06-svg@users.noreply.github.com>
2025-02-20 00:02:46 +0530

    Create MINPIZZA.cpp
    Solution for the problem MINPIZZA

---
+++ MINPIZZA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	   int n,x;
+	   cin>>n>>x;
+	   if(n*x%4!=0)
+	   cout<<((n*x)/4)+1<<endl;
+	   else
+	   cout<<(n*x)/4<<endl;
+	}
+	return 0;
+}
