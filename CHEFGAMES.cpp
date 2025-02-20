6e5253af8723908c957edfd477708fb3f345586d
Khushi Pal <65907675+khushi06-svg@users.noreply.github.com>
2025-02-21 01:30:52 +0530

    Create CHEFGAMES.cpp
    Solution for the problem CHEFGAMES

---
+++ CHEFGAMES.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int kaushal;
+	cin>>kaushal;
+	while(kaushal--){
+	    int one;
+	    cin>>one;
+	    
+	    int two;
+	    cin>>two;
+	    
+	    int three;
+	    cin>>three;
+	    
+	    int four;
+	    cin>>four;
+	    
+	      if(one==1 ||two==1 || three==1 || four==1) 
+	    {
+	       puts("OUT \n");
+	        
+	    }
+	    else {
+	        puts("IN \n");
+	        
+	    }
+	}
+	return 0;
+}
