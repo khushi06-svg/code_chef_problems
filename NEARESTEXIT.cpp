5dc2cb31b501de4fc148d032580816a8b83f4347
Khushi Pal <65907675+khushi06-svg@users.noreply.github.com>
2025-03-05 22:19:34 +0530

    Create NEARESTEXIT.cpp
    Solution for the problem NEARESTEXIT

---
+++ NEARESTEXIT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x<=50) cout<<"LEFT\n";
+	    else cout<<"RIGHT\n";
+	}
+	return 0;
+}
